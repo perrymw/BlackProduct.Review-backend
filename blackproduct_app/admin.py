c99139e2f642976e008a8469b361a68f92b2caa1
Zachary Kline <zkline888@gmail.com>
2020-08-26 22:02:58 -0400

    Added the basic viewsets of the current models for the BlackProduct.Review app.

--- blackproduct_app/admin.py
+++ blackproduct_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from blackproduct_app import models
 # Register your models here.
+
+
+admin.site.register(models.BusinessAddress)
+admin.site.register(models.Business)
+admin.site.register(models.BPRUser)
+admin.site.register(models.Reviews)
+admin.site.register(models.Product)
+admin.site.register(models.Comment)
--- blackproduct_app/views.py
+++ blackproduct_app/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from rest_framework import viewsets
+from blackproduct_app import models, serializers
+
 
 # Create your views here.
+
+
+class BusinessAddressViewSet(viewsets.ModelViewSet):
+    queryset = models.BusinessAddress.all()
+    serializer_class = serializers.BusinessAddressSerializer
+
+
+class BusinessSerializerViewSet(viewsets.ModelViewSet):
+    queryset = models.Business.all()
+    serializer_class = serializers.BusinessSerializer
+
+
+class BPRUserViewSet(viewsets.ModelViewSet):
+    queryset = models.BPRUser.all()
+    serializer_class = serializers.BPRUserSerializer
+
+
+class ReviewsViewSet(viewsets.ModelViewSet):
+    queryset = models.Reviews.all()
+    serializer_class = serializers.ReviewsSerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = models.Product.all()
+    serializer_class = serializers.ProductSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = models.Comment.all()
+    serializer_class = serializers.CommentSerializer
